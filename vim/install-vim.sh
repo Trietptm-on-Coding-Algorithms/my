fb7d40634aa445fb8ad915e5c770cef4a13af9d0
Matthew Lee <mattxlee@gmail.com>
2014-01-07 10:51:53 +0800

    fixed install-vim.sh, downloading, compiling and installing smooth

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -2,19 +2,26 @@
 
 OUTPUT_DIR=~/vim-source-tmp-dir
 VIMDIR=vim74
+VIM_PLUGINS=~/.vim
 VERSION=7.4
 PATCH_FILE="$PWD/patch-for-vim74.diff"
 
 # copy .vimrc
 cp .vimrc ~/.vimrc
 
+# checkout .vim
+[ -d $VIM_PLUGINS ] && sudo rm -rf $VIM_PLUGINS
+git clone https://github.com/mattxlee/vim $VIM_PLUGINS
+cd $VIM_PLUGINS
+git submodule update --init
+
 # make temporary directory
-[ -d $OUTPUT_DIR ] && rm -rf $OUTPUT_DIR
+[ -d $OUTPUT_DIR ] && sudo rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 cd $OUTPUT_DIR
 
 # download
-[ -e vim-$VERSION.tar.bz2 ] && rm vim-$VERSION.tar.bz2
+[ -e vim-$VERSION.tar.bz2 ] && sudo rm vim-$VERSION.tar.bz2
 wget ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2
 
 # extract
@@ -23,4 +30,7 @@ cd $VIMDIR
 
 # compile source code
 patch -p1 < $PATCH_FILE
-./configure --with-features=huge --enable-pythoninterp=yes && make install
+./configure --with-features=huge --enable-pythoninterp=yes && sudo make install
+
+# removing temporary directory
+sudo rm -rf $OUTPUT_DIR
