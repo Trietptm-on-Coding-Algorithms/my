8f8f214cb524c6d62951b349e35c5b0804fb5634
Matthew Lee <mattxlee@gmail.com>
2014-03-09 17:51:10 +0800

    modified install-xcode-theme.sh, do not remove old themes

--- xcode/install-xcode-themes.sh
+++ xcode/install-xcode-themes.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-rm ~/Library/Developer/Xcode/UserData/FontAndColorThemes/*
 cp xcode-themes/xcode-themes/* ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 chmod -x ~/Library/Developer/Xcode/UserData/FontAndColorThemes/*
