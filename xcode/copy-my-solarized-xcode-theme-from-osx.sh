8d84bb8bc4ef32e7038bb217ebc87c62a24a320b
Matthew Lee <mattxlee@gmail.com>
2014-03-11 17:26:03 +0800

    add dark theme of solarized

--- xcode/copy-my-solarized-xcode-theme-from-osx.sh
+++ xcode/copy-my-solarized-xcode-theme-from-osx.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Solarized\ -\ Dark.dvtcolortheme my-solarized-xcode-theme/
 cp ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Solarized\ -\ Light.dvtcolortheme my-solarized-xcode-theme/
