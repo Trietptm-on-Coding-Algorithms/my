0cb2d224c743d8a8cd594bbaa3b66442576ccb06
Matthew Lee <mattxlee@gmail.com>
2014-03-13 12:08:30 +0800

    modified vim install script

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -11,8 +11,7 @@ mkdir $OUTPUT_DIR
 cd $OUTPUT_DIR
 
 # download
-[ -e vim-$VERSION.tar.bz2 ] && sudo rm vim-$VERSION.tar.bz2
-wget ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2
+[ -ne vim-$VERSION.tar.bz2 ] && wget ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2
 
 # extract
 tar xf vim-$VERSION.tar.bz2
@@ -21,6 +20,3 @@ cd $VIMDIR
 # compile source code
 patch -p1 < $PATCH_FILE
 ./configure --with-features=huge --enable-pythoninterp=yes && sudo make install
-
-# removing temporary directory
-sudo rm -rf $OUTPUT_DIR
