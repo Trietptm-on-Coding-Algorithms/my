2b4f752dfb86cdc928bc53763c16f4c232cdf1af
Matthew Lee <mattxlee@gmail.com>
2014-03-20 15:32:18 +0800

    remove sudo when erasing old .vim directory

--- vim/install-vim-plugins.sh
+++ vim/install-vim-plugins.sh
@@ -3,7 +3,7 @@
 VIM_PLUGINS=~/.vim
 
 # checkout .vim
-[ -d $VIM_PLUGINS ] && sudo rm -rf $VIM_PLUGINS
+[ -d $VIM_PLUGINS ] && rm -rf $VIM_PLUGINS
 git clone https://github.com/mattxlee/vim $VIM_PLUGINS
 cd $VIM_PLUGINS
 git submodule update --init
